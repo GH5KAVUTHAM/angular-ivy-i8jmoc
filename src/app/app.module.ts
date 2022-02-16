d32255ef9b482c993c7f76a4cadd146acb6f3c34
GH5KAVUTHAM <kavutham04@gmail.com>
2022-02-16 09:42:16 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ExampleComponent } from '../example/example.component';
-import { Example2Component } from '../example2/example2.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
