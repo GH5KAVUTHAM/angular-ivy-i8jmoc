9a5a03ef8fda89f28016df40579afd4ee76ae2c4
GH5KAVUTHAM <kavutham04@gmail.com>
2022-02-16 12:57:09 +0530

    2 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent],
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot([
+    {path:'',component:ProductListComponent}
+  ]) ],
+  declarations: [ AppComponent, HelloComponent,ProductListComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
