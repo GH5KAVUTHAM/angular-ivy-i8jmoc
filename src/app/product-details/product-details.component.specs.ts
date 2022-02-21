b14373128bd35243954e5895dde9773d9d5e8cd2
GH5KAVUTHAM <kavutham04@gmail.com>
2022-02-21 23:07:51 +0530

    1 file modified

--- src/app/product-details/product-details.component.specs.ts
+++ src/app/product-details/product-details.component.specs.ts
@@ -1,24 +1,13 @@
-import { ComponentFixture } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ProductDetailsComponent } from "./product-details.component"
 
 describe('ProductDetailsComponent', () =>{
   let component: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
-  beforeEach(async () => {
+ 
+  beforeEach(async () =>{
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailsComponent ]
+      declarations: [ProductDetailsComponent]
     })
     .compileComponents();
   });
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProductDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-
-
